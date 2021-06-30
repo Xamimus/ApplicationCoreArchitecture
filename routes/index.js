7f26b8d3ffab3b5f078b26fcf9b51d53a6c06e50
Clavier Eliott <clavier.eliott.el@gmail.com>
2021-06-30 10:55:40 +0200

    Début Mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const {MongoClient} = require("mongodb");
+const photo = process.argv[2];
+const URI = "mongodb://localhost:27017/"
+
+MongoClient.connect(
+  URI,
+  {
+    useUnifiedTopology: true,
+  },
+)
+  .then(client => {
+
+    const photos = client.db('carrousel').collection('photo');
+
+    router.get('/', function(req, res, next) {
+
+      photos.find().toArray((err, items) => {
+        res.render('index', { title: 'Express', photos: items });
+      })
+
+    });
+
+    router.post('/', (req, res, next) => {
+
+      photos.insertOne({"name": "test"})
+        .then(result => {
+          console.log(result)
+        })
+        .catch(error => console.error(error))
+
+      res.render('index', { title: 'Express', photos: photos });
+
+    });
+
 });
+
 
 module.exports = router;
