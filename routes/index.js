70fba24be127a48bf0512eede554b1dbb6262929
Clavier Eliott <clavier.eliott.el@gmail.com>
2021-06-30 11:52:52 +0200

    Upload image

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var multer = require("multer");
 var router = express.Router();
 
 const {MongoClient} = require("mongodb");
-const photo = process.argv[2];
 const URI = "mongodb://localhost:27017/"
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null,  file.originalname );
+  }
+});
+var upload = multer({ storage: storage });
 
 MongoClient.connect(
   URI,
@@ -16,23 +26,22 @@ MongoClient.connect(
     const photos = client.db('carrousel').collection('photo');
 
     router.get('/', function(req, res, next) {
-
       photos.find().toArray((err, items) => {
         res.render('index', { title: 'Express', photos: items });
       })
-
     });
 
-    router.post('/', (req, res, next) => {
+    router.get('/upload', (req, res, next) => {
+      res.redirect('/');
+    })
 
-      photos.insertOne({"name": "test"})
+    router.post('/upload', upload.single('image'), (req, res, next) => {
+      photos.insertOne({"image": req.file.originalname})
         .then(result => {
-          console.log(result)
+          return true;
         })
         .catch(error => console.error(error))
-
-      res.render('index', { title: 'Express', photos: photos });
-
+      res.redirect('/');
     });
 
 });
